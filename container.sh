f1866aa780611c63ed3d7e77a42824b1123a88a7
venrodr <77059955+venrodr@users.noreply.github.com>
2021-01-12 21:03:53 +0800

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,6 +1,7 @@
 docker run -dt --name testcontainer testimage:latest /bin/bash
 docker container ls
 docker exec testcontainer ls
+docker exec testcontainer cat eicar.com
 docker kill testcontainer
 docker rm testcontainer
 docker container ls
